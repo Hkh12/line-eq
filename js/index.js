ac53648024bb054b4a8f27b74d6452536300fbcb
Hosein Khansari <hrk.x11@gmail.com>
2019-06-08 16:11:21 +0430

    Use Spreax ✨

--- js/index.js
+++ js/index.js
@@ -1,20 +1,24 @@
+import { Spreax } from 'spreax/dist/spreax.esm';
 import parse from "./parser";
 
-const path = document.querySelector("path.line");
-const errors = document.querySelector("#error");
-const form = document.querySelector("form");
-
-form.addEventListener("submit", ev => {
-    ev.preventDefault();
-    try {
-        errors.innerHTML = "";
-        const { slope, x, y } = parse(form.elements.equation.value);
-
-        const d = slope === Infinity ? `M ${150 + x} 0 l 0 300` :
-            slope === 0 ? `M 0 ${150 - y} l 300 0` :
-                `M ${150 + x} ${150 - y} l ${150 - x} ${-slope * 150 - y} l -300 ${slope * 300}`;
-        path.setAttribute("d", d);
-    } catch ({ message: m }) {
-        errors.innerHTML = m;
+const app = new Spreax({
+    el: document.querySelector('#app'),
+    state: {
+        eq: '',
+        error: false,
+        d: ''
     }
-});
+})
+
+app.$ctx.$on('eq', ({ value }) => {
+    try {
+        const { slope, x, y } = parse(value),
+            d = slope === Infinity ? `M ${150 + x} 0 l 0 300` :
+                slope === 0 ? `M 0 ${150 - y} l 300 0` :
+                    `M ${150 + x} ${150 - y} l ${150 - x} ${-slope * 150 - y} l -300 ${slope * 300}`;
+        app.d = d
+        app.error = false
+    } catch ({ message }) {
+        app.error = message
+    }
+})
